975e8e76b1c4388d6976d4049d4675d30ee47727
Juan Pablo Buritica <buritica@gmail.com>
2014-08-26 02:10:44 -0500

    [add] test for GET /notas/

--- test/notas.js
+++ test/notas.js
@@ -1,6 +1,7 @@
 var request = require('supertest-as-promised');
 var api = require('../server.js');
 var host = process.env.API_TEST_HOST || api;
+var _ = require('lodash');
 
 request = request(host);
 
@@ -45,7 +46,7 @@ describe('Coleccion de Notas [/notas]', function() {
     });
   });
 
-  describe('GET', function() {
+  describe('GET /notas/:id', function() {
     it('deberia obtener una nota existente', function(done) {
       var id;
       var data = {
@@ -192,6 +193,84 @@ describe('Coleccion de Notas [/notas]', function() {
       .then(function confirmation(res) {
         var body = res.body;
         expect(body).to.be.empty;
+        done();
+      }, done);
+
+    });
+  });
+
+  describe('GET /notas/', function() {
+    it('deberia obtener todas las notas existente', function(done) {
+      var id1;
+      var id2;
+
+      var data1 = {
+        "nota": {
+          "title": "Mejorando.la #node-pro",
+          "description": "Introduccion a clase",
+          "type": "js",
+          "body": "soy el cuerpo de json"
+        }
+      };
+
+      var data2 = {
+        "nota": {
+          "title": "Mejorando.la #node-pro",
+          "description": "Ya casi se acaba esta clase",
+          "type": "ruby",
+          "body": "wohooooooooo"
+        }
+      };
+
+      request
+        .post('/notas')
+        .set('Accept', 'application/json')
+        .send(data1)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      .then(function createAnotherNota(res) {
+        id1 = res.body.nota.id;
+        return request
+          .post('/notas')
+          .set('Accept', 'application/json')
+          .send(data2)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
+      })
+      .then(function getNotas(res) {
+        id2 = res.body.nota.id;
+        return request.get('/notas/')
+          .set('Accept', 'application/json')
+          .send()
+          .expect(200)
+          .expect('Content-Type', /application\/json/)
+      }, done)
+      .then(function assertions(res) {
+        var body = res.body;
+
+        expect(body).to.have.property('notas');
+        expect(body.notas).to.be.an('array')
+          .and.to.have.length.above(2);
+
+        var notas = body.notas;
+
+        var nota1 = _.find(notas, {id:id1});
+        var nota2 = _.find(notas, {id:id2});
+
+        // Propiedades nota1
+        expect(nota1).to.have.property('id', id1);
+        expect(nota1).to.have.property('title', 'Mejorando.la #node-pro');
+        expect(nota1).to.have.property('description', 'Introduccion a clase');
+        expect(nota1).to.have.property('type', 'js');
+        expect(nota1).to.have.property('body', 'soy el cuerpo de json');
+
+        // Propiedades nota1
+        expect(nota2).to.have.property('id', id2);
+        expect(nota2).to.have.property('title', 'Mejorando.la #node-pro');
+        expect(nota2).to.have.property('description', 'Ya casi se acaba esta clase');
+        expect(nota2).to.have.property('type', 'ruby');
+        expect(nota2).to.have.property('body', 'wohooooooooo');
+
         done();
       }, done);
 
